346cd26c070b1088da4978ee384b7d4f5c4b45f2
pgsr91 <pgsr91@hotmail.com>
2017-05-27 01:36:27 +0530

    Assignment 1.1

--- Assignment1.java
+++ Assignment1.java
@@ -1,19 +1,20 @@
 package Session;
 
-import java.util.Scanner;
+import java.util.Scanner; //Importing Scanner class.
 
-public class Assignment1 {
+public class Assignment1 
+{
 
 	public static void main(String arg[])
 	{
-		int num1, num2, sum;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first integer:  ");
-	num1=sc.nextInt();
+		int num1, num2, sum; //Declaring required variables.
+		Scanner sc=new Scanner(System.in); //Using object of class Scanner for input from user.
+		System.out.println("Enter first integer:  "); 
+	num1=sc.nextInt(); // Getting value for first integer.
 	System.out.println("Enter second integer:  ");
-	num2=sc.nextInt();
+	num2=sc.nextInt(); // Getting value for second integer.
 	sum=num1-(-num2);
-	System.out.println("Sum of "+num1+" and "+num2+" is "+sum);
+	System.out.println("Sum of "+num1+" and "+num2+" is "+sum); //Printing sum of both integer numbers.
 		
 	}
 }
